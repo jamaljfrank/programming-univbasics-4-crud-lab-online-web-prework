3b06ac4a7142f3d0a0783b928ec361ec0be103e9
Jamal Frank <jamaljfrank@gmail.com>
2019-07-06 00:28:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,31 +3,31 @@ def create_an_empty_array
 end
 
 def create_an_array
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  ["Selkie", "Clavat", "Lilty", "Yuke"]
 end
 
 def add_element_to_end_of_array(array, element)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   race.push("Human")
 end
 
 def add_element_to_start_of_array(array, element)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   race.unshift("Human")
 end
 
 def remove_element_from_end_of_array(array)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   "Yuke" = race.pop
 end
 
 def remove_element_from_start_of_array(array)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   "Selkie" = race.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   race[1]
 end
 
