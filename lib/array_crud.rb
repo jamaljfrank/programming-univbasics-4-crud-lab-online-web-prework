c932390aff271d49c6883efc9ca05860658286ca
Jamal Frank <jamaljfrank@gmail.com>
2019-07-06 00:38:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   
-  array.pop = (learning)
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
   
-  array.shift = ("Wow")
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
