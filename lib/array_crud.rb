84ca03ebc6a30ead10a2b3c7e4280b77761f9e14
Jamal Frank <jamaljfrank@gmail.com>
2019-07-06 00:30:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,18 +12,18 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
-  race.unshift("Human")
+  
+  race.unshift
 end
 
 def remove_element_from_end_of_array(array)
   
-  "Yuke" = race.pop
+  race.pop
 end
 
 def remove_element_from_start_of_array(array)
   
-  "Selkie" = race.shift
+  race.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -37,11 +37,11 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   race[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  race = ["Selkie", "Clavat", "Lilty", "Yuke"]
+  
   race[3] = please_work
 end
